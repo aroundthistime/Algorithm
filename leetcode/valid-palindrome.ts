ad1ff16c5c5350791c7b59c4c26015244e420fb2
aroundthistime <dongdhy@naver.com>
2025-09-12 21:45:39 +0900

    feat: palindrome - use regex for exec time optimization

--- leetcode/valid-palindrome.ts
+++ leetcode/valid-palindrome.ts
@@ -2,22 +2,13 @@
  * @link https://leetcode.com/problems/valid-palindrome/
  */
 function isPalindrome(s: string): boolean {
-  const MIN_ALPHABET_CHAR_CODE = 'a'.charCodeAt(0);
-  const MAX_ALPHABET_CHAR_CODE = 'z'.charCodeAt(0);
-
-  const filteredLowercaseStr = s.toLowerCase().split('').filter((letter) => {
-      if (letter === ' ') return;
-      const charCode = letter.charCodeAt(0);
-      const isAlphabet = charCode >= MIN_ALPHABET_CHAR_CODE && charCode <= MAX_ALPHABET_CHAR_CODE;
-      const isNumeric = !Number.isNaN(Number(letter));
-      return isAlphabet || isNumeric
-  }).join('');
+  const alphanumericOnlyStr = s.replace(/[^a-zA-Z0-9]/g, '');
 
   let l = 0;
-  let r = filteredLowercaseStr.length - 1;
+  let r = alphanumericOnlyStr.length - 1;
 
   while (l <= r) {
-      if (filteredLowercaseStr[l] !== filteredLowercaseStr[r]) return false;
+      if (alphanumericOnlyStr[l].toLowerCase() !== alphanumericOnlyStr[r].toLowerCase()) return false;
       l += 1;
       r -= 1;
   }
