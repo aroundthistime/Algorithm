d60cab2184e58f9d07186d2272358ae077bd35ea
aroundthistime <dongdhy@naver.com>
2025-09-13 20:13:17 +0900

    feat: update longest palindromic substring (25ms -> 21ms)
    calculate the optimistic max palindrome length to prevent unnecessary expansion in advance

--- leetcode/longest-palindromic-substring.ts
+++ leetcode/longest-palindromic-substring.ts
@@ -22,25 +22,38 @@ function longestPalindrome(s: string): string {
       return s.slice(l, r + 1)
   }
 
+  /**
+   * Calculate the maximum possible palindrome length for a given left and right index
+   * Used to prevent unnecessary palindrome expansion
+   */
+  const getMaxPossiblePalindromeLength = (l: number, r: number) => {
+    const basicLength = r - l + 1;
+    const leftMaxExpand = l;
+    const rightMaxExpand = s.length - 1 - r;
+    return Math.min(leftMaxExpand, rightMaxExpand) * 2 + basicLength;
+  }
+
+
   let answer = '';
 
   for (let i = 0; i < s.length; i++) {
-      /**
-       * palindrome에는 크게 두 가지 종류가 있음
-       * 짝수 개수여서 두 개가 같을 수도 있고
-       * 홀수여서 가운데꺼 제외하고 양쪽이 같을 수도 있음
-       */
-      const oddLengthLongestPalindrome = getMaxExpandablePalindrome(i, i);
-      if (oddLengthLongestPalindrome.length > answer.length) {
-          answer = oddLengthLongestPalindrome;
-      }
+    const maxOddPossibleLength = getMaxPossiblePalindromeLength(i, i);
+    if (answer.length < maxOddPossibleLength) {
+        const oddLengthLongestPalindrome = getMaxExpandablePalindrome(i, i);
+        if (oddLengthLongestPalindrome.length > answer.length) {
+            answer = oddLengthLongestPalindrome;
+        }
+    }
 
-      if (s[i] === s[i + 1]) {
-          const evenLengthLongestPalindrome = getMaxExpandablePalindrome(i, i + 1);
-          if (evenLengthLongestPalindrome.length > answer.length) {
-              answer = evenLengthLongestPalindrome;
-          }
-      }
+    if (s[i] === s[i + 1]) {
+        const maxEvenPossibleLength = getMaxPossiblePalindromeLength(i, i + 1);
+        if (answer.length < maxEvenPossibleLength) {
+            const evenLengthLongestPalindrome = getMaxExpandablePalindrome(i, i + 1);
+            if (evenLengthLongestPalindrome.length > answer.length) {
+                answer = evenLengthLongestPalindrome;
+            }
+        }
+    }
   }
 
   return answer
